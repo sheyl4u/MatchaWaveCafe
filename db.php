297f33d72c0702958c3a4cbe37f4d3dcbd562a86
Niawra <541251286@student.smktelkom-pwt.sch.id>
2026-05-23 02:17:31 +0700

    update project

--- db.php
+++ db.php
@@ -14,3 +14,11 @@ if(!$conn){
 }
 
 ?>
+
+<?php
+
+$conn = new mysqli("localhost", "root", "", "matchawave");
+
+if ($conn->connect_error) {
+    die("Koneksi gagal: " . $conn->connect_error);
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>MatchaWave Payment</title>
+
+<script src="https://app.sandbox.midtrans.com/snap/snap.js"
+data-client-key="Mid-client-_4-QMLHAwzVUVfC_"></script>
