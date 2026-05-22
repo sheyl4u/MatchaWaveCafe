bd093dd11c8d4bf0c6e4b3e82ce9bba10eb7e45e
Niawra <541251286@student.smktelkom-pwt.sch.id>
2026-05-23 04:31:48 +0700

    update project

--- checkout.php
+++ checkout.php
@@ -6,33 +6,91 @@ ini_set('display_errors', 1);
 require_once __DIR__ . '/vendor/autoload.php';
 require_once 'db.php';
 
+/* =========================================
+   MIDTRANS CONFIG
+========================================= */
+
 \Midtrans\Config::$serverKey = 'Mid-server-CrTdnigRuLCIl34SLTt9CEVO';
 \Midtrans\Config::$isProduction = false;
 \Midtrans\Config::$isSanitized = true;
 \Midtrans\Config::$is3ds = true;
 
+/* =========================================
+   AMBIL DATA DARI FETCH JS
+========================================= */
+
+$data = json_decode(
+    file_get_contents("php://input"),
+    true
+);
+
+$name   = $data['name'];
+$email  = $data['email'];
+$phone  = $data['phone'];
+$amount = $data['total'];
+
+/* =========================================
+   BUAT ORDER ID
+========================================= */
+
 $order_id = "ORDER-" . time();
 
-$name = "Budi";
-$email = "budi@email.com";
-$amount = 10000;
+/* =========================================
+   SIMPAN KE DATABASE
+========================================= */
 
 $conn->query("
-INSERT INTO orders(order_id,name,email,amount,status)
-VALUES('$order_id','$name','$email','$amount','pending')
+
+INSERT INTO orders(
+order_id,
+name,
+email,
+amount,
+status
+)
+
+VALUES(
+'$order_id',
+'$name',
+'$email',
+'$amount',
+'pending'
+)
+
 ");
 
+/* =========================================
+   DATA MIDTRANS
+========================================= */
+
 $params = array(
+
     'transaction_details' => array(
         'order_id' => $order_id,
         'gross_amount' => $amount,
     ),
+
     'customer_details' => array(
         'first_name' => $name,
         'email' => $email,
+        'phone' => $phone
     )
+
 );
 
-$snapToken = \Midtrans\Snap::getSnapToken($params);
+/* =========================================
+   GENERATE SNAP TOKEN
+========================================= */
 
-echo $snapToken;
+$snapToken =
+\Midtrans\Snap::getSnapToken($params);
+
+/* =========================================
+   KIRIM TOKEN KE JS
+========================================= */
+
+echo json_encode([
+    "token" => $snapToken
+]);
+
+?>
--- db.php
+++ db.php
@@ -12,13 +12,3 @@ if(!$conn){
     die("Koneksi database gagal!");
 
 }
-
-?>
-
-<?php
-
-$conn = new mysqli("localhost", "root", "", "matchawave");
-
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
